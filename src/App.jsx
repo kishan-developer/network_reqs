24bdce5a23260e3c96ecdd8925fe566310faf1db
github_demo_2 <gunnikij1665@gmail.com>
2023-12-12 13:40:27 +0530

    Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import GetAxios from './Components/GetAxios'
-import PostAxios from './Components/PostAxios'
+// import GetAxios from './Components/GetAxios'
+// import PostAxios from './Components/PostAxios'
 import CancelNetworkReq from './Components/CancelNetworkReq'
 
 function App() {
   
   return (
-    <div>
+    <div className='p-50 bg-gray-300'>
      {/* <p> All type network request get,post using axios</p> */}
 
       
